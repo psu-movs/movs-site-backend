a5cb444d6a4f9cda78fdc182db9f9f59cecda2dd
Damego <danyabatueff@gmail.com>
2023-06-08 19:19:03 +0500

    feat: add cors middleware

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from beanie import init_beanie
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
+from starlette.middleware.cors import CORSMiddleware
 
 from schemas import User, Teacher, DepartmentHead, DepartmentInfo, Article
 from routes import users, teachers, department_info, news
@@ -23,3 +24,12 @@ app.include_router(news.router)
 async def start():
     db_client = AsyncIOMotorClient(environ["MONGODB_URL"])
     await init_beanie(database=db_client.movs, document_models=[User, Teacher, DepartmentHead, DepartmentInfo, Article])
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allow_headers=["Content-Type", "Authorization"],
+)
