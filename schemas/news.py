8970716faba9bc8680669364ff0bb0706b612ff7
Damego <damego.dev@gmail.com>
2023-06-11 14:16:40 +0500

    refactor: return article with id

--- schemas/news.py
+++ schemas/news.py
@@ -1,17 +1,11 @@
 from beanie import Document
 from pydantic import BaseModel
 
-class ArticleWithoutDescription(BaseModel):
-    author_id: str
-    title: str
-    creation_date: str
-    thumbnail_url: str
-    thumbnail_file_id: str
 
 class Article(Document):
     author_id: str
     title: str
-    description: str
+    description: str | None = None
     creation_date: str
     thumbnail_url: str | None = None
     thumbnail_file_id: str | None = None
